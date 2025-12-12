db0d9cb6e6b2105ec3a888974bcc074add55bd0c
Dslpss <dennisemannuel93@gmail.com>
2025-12-12 10:19:11 -0300

    Correção de posicionamente do bt de IA

--- src/components/FloatingAIButton.tsx
+++ src/components/FloatingAIButton.tsx
@@ -68,7 +68,7 @@ const styles = StyleSheet.create({
   container: {
     position: 'absolute',
     bottom: 100,
-    right: 20,
+    left: 20,
     zIndex: 1000,
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.3,
